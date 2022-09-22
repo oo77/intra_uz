41c2c990cf4993b948fa9afffb141e567fd0d748
fobus1289 <fobus1289@mai.ru>
2022-09-22 10:42:30 +0500

    fix scroll page

--- src/components/slidersComponent/TextContentStyle.vue
+++ src/components/slidersComponent/TextContentStyle.vue
@@ -1,10 +1,20 @@
 <template>
-  <p class="text-center c-#39B44A text-26px font-700 mb-3 " lg=" text-32px" v-html="title"/>
+  <p ref="me" class="text-center c-#39B44A text-26px font-700 mb-3 " lg=" text-32px" v-html="title"/>
   <p class="text-justify text-16px mb-5" lg="text-20px  mx-0 " v-html="content"/>
 </template>
 
 <script setup>
-import {defineProps} from "vue";
-const {title, content} = defineProps({title:String, content: String})
+import {defineProps, ref, defineEmits, onMounted} from "vue";
+import {useRouter} from "vue-router/dist/vue-router";
+
+const {title, content} = defineProps({title: String, content: String})
+const router = useRouter()
+const me = ref()
+const emits = defineEmits(['isLab'])
+
+onMounted(() => {
+  emits('isLab', me.value)
+})
+
 </script>
 
