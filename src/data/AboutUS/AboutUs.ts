e9f5d45a175148756b816591ea324d57c67e313b
ozodbek Abduraximov <procab.uzkabel@gmail.com>
2022-09-30 22:56:42 +0500

    fix

--- src/data/AboutUS/AboutUs.ts
+++ src/data/AboutUS/AboutUs.ts
@@ -1,7 +1,6 @@
 import AboutCompanyMainImg from 'assets/icons/aboutCompany.jpg'
 
-const Content = {
-  aboutCompany: {
+ const aboutCompany= {
     title: {
       ru: "О Компании ",
       en: "About the company",
@@ -10,17 +9,19 @@ const Content = {
     },
     description: {
       ru: `Добро пожаловать, мы рады приветствовать Вас на нашем сайте! Мы являемся крупнейшим мясным агрокластером, занимающийся разведением, выращиванием и переработкой мясных пород. <br><br>Наша компания является официальным партнёром «Национальной Ассоциации Кролиководов Италия (ANCI)» в Центральной Азии и в странах СНГ, занимающееся разведением итальянских пород кроликов.<br><br> Агрокомплекс «Моя Мечта» является крупнейшим в мире кролиководческим кластером, расположенный в экологически чистой зоне Ташкентской области, спроектированный итальянскими специалистами по передовым технологиям, отвечающим всем европейским стандартам.<br><br>На основании прав, предоставленных «Национальной Ассоциации Кролиководов Италии (ANCI)», мы занимаемся производством и реализацией генетики кроликов, кроликоматок, кроличьего мяса из представленного генетического кода чистокровных пород — кроличьих прародителей. <br><br>Наш агрокомплекс содержит в себе полный цикл необходимых процессов для полноценного ведения кролиководческой деятельности:
-<ul style=" padding-left: 3rem; margin-bottom: 0px; text-align: left"> <li>   генетический центр/лаборатория, позволяющая создавать и выращивать мясные породы кроликов путём искусственного осеменения;</li> <li>   кормовой завод, обеспечивающий полноценное,   сбалансированное и витаминосодержащее питание кроликов;</li> <li>   бойня, предоставляющая возможность быстрой, качественной переработки мясных изделий, получаемых при разведении кроликов;</li> <li>   компостный завод, производящий органические удобрения высокой продуктивности.</li></ul> <br><br>Почему именно Италия? <br><br>Кролиководы из Италии на протяжении более 40 лет изучают кроличьи породы высокого качества и выводят их генетику, улучшая показатели устойчивости к    различным заболеваниям и погодным условиям, сохраняя качество и полезные свойства кроличьего мяса.`,
-
-      uz: `Assalomu alaykum, biz sizni saytimizda kutib olishdan xursandmiz! Biz goʻsht navlarini koʻpaytirish, yetishtirish va qayta ishlash bilan shugʻullanadigan eng yirik goʻsht agroklasteri hisoblanamiz.<br><br>Kompaniyamiz «Italiya quyon yetishtiruvchilar Milliy Assotsiatsiyasi (ANCI)«ning Markaziy Osiyo va MDH mamlakatlarida Italiya quyon zotlarini yetishtiruvchi rasmiy hamkori hisoblanadi. <br><br>«Моя Мечта» agrokompleksi Toshkent viloyatining ekologik toza zonasida joylashgan, italiyalik mutaxassislar tomonidan butun Yevropa standartlariga javob beradigan ilgʻor texnologiyalar boʻyicha ishlab chiqilgan, dunyodagi eng yirik quyonchilik klasteridir.<br><br>«Italiya quyon yetishtiruvchilar Milliy Assotsiatsiyasi (ANCI)” ga berilgan huquqlardan kelib chiqib, biz taqdim etilgan asl zotli quyon avlodlarining genetik kodidan quyonlar, urgʻochi quyonlar genetikasini va albatta quyon goʻshtini ishlab chiqarish va sotish bilan shugʻullanamiz.<br><br>Bizning agrokompleksimiz quyonchilik faoliyatini toʻlaqonli olib borish uchun zarur jarayonlarning toʻliq siklini oʻz ichiga oladi:
 <ul style=" padding-left: 3rem; margin-bottom: 0px; text-align: left">
+<li>   генетический центр/лаборатория, позволяющая создавать и выращивать мясные породы кроликов путём искусственного осеменения;</li>
+<li>   кормовой завод, обеспечивающий полноценное,   сбалансированное и витаминосодержащее питание кроликов;</li>
+<li>   бойня, предоставляющая возможность быстрой, качественной переработки мясных изделий, получаемых при разведении кроликов;</li>
+<li>   компостный завод, производящий органические удобрения высокой продуктивности.</li>
+</ul> <br><br>Почему именно Италия? <br><br>Кролиководы из Италии на протяжении более 40 лет изучают кроличьи породы высокого качества и выводят их генетику, улучшая показатели устойчивости к    различным заболеваниям и погодным условиям, сохраняя качество и полезные свойства кроличьего мяса.`,
+
+      uz: `Assalomu alaykum, biz sizni saytimizda kutib olishdan xursandmiz! Biz goʻsht navlarini koʻpaytirish, yetishtirish va qayta ishlash bilan shugʻullanadigan eng yirik goʻsht agroklasteri hisoblanamiz.<br><br>Kompaniyamiz "Italiya quyon yetishtiruvchilar Milliy Assotsiatsiyasi (ANCI)" ning Markaziy Osiyo va MDH mamlakatlarida Italiya quyon zotlarini yetishtiruvchi rasmiy hamkori hisoblanadi. <br><br>«Моя Мечта» agrokompleksi Toshkent viloyatining ekologik toza zonasida joylashgan, italiyalik mutaxassislar tomonidan butun Yevropa standartlariga javob beradigan ilgʻor texnologiyalar boʻyicha ishlab chiqilgan, dunyodagi eng yirik quyonchilik klasteridir.<br><br>«Italiya quyon yetishtiruvchilar Milliy Assotsiatsiyasi (ANCI)” ga berilgan huquqlardan kelib chiqib, biz taqdim etilgan asl zotli quyon avlodlarining genetik kodidan quyonlar, urgʻochi quyonlar genetikasini va albatta quyon goʻshtini ishlab chiqarish va sotish bilan shugʻullanamiz.<br><br>Bizning agrokompleksimiz quyonchilik faoliyatini toʻlaqonli olib borish uchun zarur jarayonlarning toʻliq siklini oʻz ichiga oladi:<ul style=" padding-left: 3rem; margin-bottom: 0px; text-align: left">
 <li>   sunʼiy urugʻlantirish orqali quyonlarning goʻshtli zotlarini yaratish va yetishtirish imkonini beruvchi genetik markaz/laboratoriya;</li>
 <li>   quyonlarni toʻliq, muvozanatli va vitaminli oziqlantirishni taʼminlovchi ozuqa zavodi;</li>
 <li>   quyonlarini koʻpaytirish orqali olingan goʻsht mahsulotlarini tez, sifatli qayta ishlash imkoniyatini beruvchi qassobxona; </li>
 <li>   yuqori mahsuldorlikka ega organik oʻgʻitlar ishlab chiqaradigan kompost zavodi.</li>
-</ul>
-
-<br><br>Nima uchun aynan Italiya?<br><br>Italiyalik quyon yetishtiruvchilar 40 yildan ortiq vaqt davomida yuqori sifatli quyon zotlarini oʻrganib, ularning turli kasalliklar va ob-havo sharoitlariga chidamlilik koʻrsatkichlarini yaxshilashga erishishgan, quyon goʻshtining sifati va foydali xususiyatlarini saqlab qolgan holda, ularning genetikasini chiqarib keladilar.`,
+</ul> <br><br>Nima uchun aynan Italiya?<br><br>Italiyalik quyon yetishtiruvchilar 40 yildan ortiq vaqt davomida yuqori sifatli quyon zotlarini oʻrganib, ularning turli kasalliklar va ob-havo sharoitlariga chidamlilik koʻrsatkichlarini yaxshilashga erishishgan, quyon goʻshtining sifati va foydali xususiyatlarini saqlab qolgan holda, ularning genetikasini chiqarib keladilar.`,
 
       en: `Welcome, we are glad to welcome you on our website! We are the largest meat agrocluster engaged in breeding, growing and processing meat breeds.<br><br>Our company is an official partner of the "National Association of Rabbit Breeders of Italy (ANCI)" in Central Asia and in the CIS countries, engaged in breeding Italian rabbit breeds.<br><br>Agrocomplex "My Dream" is the world's largest rabbit breeding cluster, located in an ecologically clean zone of the Tashkent region, designed by Italian specialists using advanced technologies that meet all European standards. <br><br>Based on the rights granted to the "National Association of Rabbit Breeders of Italy (ANCI)", we are engaged in the production and sale of genetics of rabbits, female rabbits, rabbit meat from the presented genetic code of purebred breeds — rabbit progenitors. <br><br>Our agrocomplex contains a full cycle of necessary processes for the full-fledged management of rabbit breeding activities:
  <ul style=" padding-left: 3rem; margin-bottom: 0px; text-align: left">
@@ -42,9 +43,9 @@ const Content = {
 <br><br>Нима учун айнан Италия?<br><br>Италиялик қуён етиштирувчилар 40 йилдан ортиқ вақт давомида юқори сифатли қуён зотларини ўрганиб, уларнинг турли касалликлар ва об-ҳаво шароитларига чидамлилик кўрсаткичларини яхшилашга эришишган, қуён гўштининг сифати ва фойдали хусусиятларини сақлаб қолган ҳолда, уларнинг генетикасини чиқариб келадилар.`,
     },
 
-  },
+  }
 
-  pride: {
+ const pride= {
     title: {
       ru: "Гордость компании",
       en: "We take pride in",
@@ -85,9 +86,9 @@ const Content = {
       },
 
     ]
-  },
+  }
 
-  partners: {
+  const partners={
     title: {
       ru: "Наши партнеры",
       en: "Our partners",
@@ -102,10 +103,11 @@ const Content = {
     }
   }
 
-}
 
 
 export {
   AboutCompanyMainImg,
-  Content
+  aboutCompany,
+  pride,
+  partners
 }
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,67 +1,65 @@
 <template>
-  <div class="z-1">
-    <q-img :src="AboutCompanyMainImg"
-           class="w-full min-h-fit"
-           fit="cover"/>
+  <q-img :src="AboutCompanyMainImg"
+         class="w-full min-h-fit"
+         fit="cover"/>
 
-    <div
-      class="container q-mx-auto my-10">
+  <div class="container q-mx-auto my-10">
 
-      <div class="mx-3%">
-        <text-content-style :content="Content.aboutCompany.description[lang.prefix]"
-                            :title="Content.aboutCompany.title[lang.prefix]"/>
+    <div class="mx-3%">
+      <div>
+        <p ref="me" class="text-center c-#39B44A text-26px font-700 mb-3 " lg="text-32px" v-html="aboutCompany.title[lang.prefix]"/>
+        <p class="text-justify text-16px mb-5" lg="text-20px  mx-0 " v-html="aboutCompany.description[lang.prefix]"/>
+      </div>
 
-        <AboutUsSlider class="mb-10 q-mx-auto"/>
+      <AboutUsSlider class="mb-10 q-mx-auto"/>
 
-        <p class="text-center c-#39B44A text-26px font-700 ma-0 mt-30px mb-10px"
-           lg="mt-50px text-32px"
-           v-html="Content.pride.title[lang.prefix]"/>
+      <p class="text-center c-#39B44A text-26px font-700 ma-0 mt-30px mb-10px"
+         lg="mt-50px text-32px"
+         v-html="pride.title[lang.prefix]"/>
 
-        <div class="mb-50px mx-8%">
+      <div class="mb-50px mx-8%">
 
-          <ul v-for="p in prides"
-              class="list-none border-b-1 border-slate-200 pa-0 flex justify-left text-left"
-              lg="justify-between text-left">
+        <div v-for="p in pride.prides.length"
+             class="list-none border-b-1 border-slate-200 pa-0 flex justify-left text-left"
+             lg="justify-between text-left">
 
-            <li class="w-90% text-16px flex items-center"
-                lg="text-20px"
-                v-html="p[lang.prefix]"/>
-            <q-img :src="proudPNG" class="w-70px h-70px gt-sm"/>
-          </ul>
-
+          <p class="w-90% text-16px flex items-center ma-0 my-2"
+             lg="text-20px my-0"
+             v-html="pride.prides[p-1][lang.prefix]"/>
+          <q-img :src="proudPNG" class="w-70px h-70px gt-sm"/>
         </div>
 
-        <text-content-style :content="Content.partners.description[lang.prefix]"
-                            :title="Content.partners.title[lang.prefix]"/>
-
-        <PartnersSlide/>
-
-        <p class="text-justify text-16px mt-5" sm="text-20px  mx-0"
-           v-html="PartnersContent.description2[lang.prefix]"/>
-
-        <Subscribe/>
       </div>
+      <div>
+        <p ref="me" class="text-center c-#39B44A text-26px font-700 mb-3 " lg="text-32px" v-html="partners.title[lang.prefix]"/>
+        <p class="text-justify text-16px mb-5" lg="text-20px  mx-0 " v-html="partners.description[lang.prefix]"/>
+      </div>
+
+      <PartnersSlide/>
+
+      <p class="text-justify text-16px mt-5" sm="text-20px  mx-0"
+         v-html="PartnersContent.description2[lang.prefix]"/>
+
+      <Subscribe/>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {AboutCompanyMainImg, Content} from "src/data/AboutUS/AboutUs";
+import {aboutCompany, AboutCompanyMainImg, partners, pride} from "src/data/AboutUS/AboutUs";
 import AboutUsSlider from "components/slidersComponent/GallerySlider.vue";
 import PartnersSlide from 'components/slidersComponent/PartnersSlide.vue';
 import {useLanguageStore} from "stores/lang";
 import proudPNG from 'assets/icons/proudTrue.png'
-import TextContentStyle from 'components/slidersComponent/TextContentStyle.vue'
 import {useAboutCompanyMeta} from "src/meta/about_company";
 import {PartnersContent} from "src/data/Partners/partners";
 import Subscribe from "components/slidersComponent/subscribe.vue";
 import {onActivated, watch} from "vue";
 
-const prides = Content.pride.prides
 const lang = useLanguageStore()
 
 useAboutCompanyMeta(lang.prefix)
-onActivated(()=>{
+onActivated(() => {
   watch(lang, () => useAboutCompanyMeta(lang.prefix))
 })
 
